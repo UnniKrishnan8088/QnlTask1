2ea32d6d363454deba29893b48a9b2935af213e4
Unni Krishnan <prasannaunni2001@gmail.com>
2024-05-04 20:58:59 +0530

    commit

--- src/pages/Datas.tsx
+++ src/pages/Datas.tsx
@@ -95,7 +95,6 @@ export default function Datas() {
 
   const table = useReactTable({
     data: posts,
-    // data: storedData,
     columns,
     debugTable: true,
     getCoreRowModel: getCoreRowModel(),
